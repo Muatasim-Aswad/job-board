b5654dad24bba02fda922d76a42e35484c96e309
Muatasim-Aswad <malasoad2021@gmail.com>
2025-08-14 13:25:12 +0200

    init dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { AppSidebarClient } from './_AppSidebarClient';
+
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Welcome to the Job Board</h1>
-        </div>
-    );
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Button</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">Main Content</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
